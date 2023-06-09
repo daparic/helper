beb877e563993119943a349dcdd809d5c37c855b
Your Name <you@example.com>
2023-06-09 21:17:41 +0800

    Add test1, test2 functions

--- Makefile
+++ Makefile
@@ -7,12 +7,13 @@ helper.a: helper.o
 
 helper.o: helper.cu
 	/usr/local/cuda-10.2/bin/nvcc \
+      -ccbin gcc \
       -m64 \
       -gencode arch=compute_53,code=sm_53 \
       -o helper.o -c helper.cu
 
 app: app.cpp helper.o
-	g++ -o app app.cpp helper.o  \
+	g++ -g -o app app.cpp helper.o  \
       -I/usr/local/cuda/targets/aarch64-linux/include \
       -L/usr/local/cuda/targets/aarch64-linux/lib/ \
       -lcudart 
--- app.cpp
+++ app.cpp
@@ -3,17 +3,19 @@
 
 #include <cuda_runtime.h>
 
-extern "C" void do_transpose(dim3 gDim, dim3 bDim, float *Md, float *Bd,
-                             uint WIDTH);
+extern "C" {
+void do_transpose(dim3 gDim, dim3 bDim, float *Md, float *Bd, uint WIDTH);
+void do_transpose2(float *M, uint WIDTH, uint HEIGHT);
+}
 
-int main(int args, char **vargs) {
+void test1() {
     const int HEIGHT = 5; // 1024;
     const int WIDTH = 5;  // 1024;
     const int SIZE = WIDTH * HEIGHT * sizeof(float);
     // dim3 bDim(16, 16);
     dim3 bDim(3, 3);
     dim3 gDim(WIDTH / bDim.x, HEIGHT / bDim.y);
-    float *M = (float *)malloc(SIZE);
+    float *M = new float[SIZE];
     for (int i = 0; i < HEIGHT * WIDTH; i++) {
         M[i] = i;
     }
@@ -35,7 +37,38 @@ int main(int args, char **vargs) {
         printf("%f ", M[i]);
     }
 
-    free(M);
-    printf("\n*** done ***\n");
+    delete (M);
+    printf("\n*** done1 ***\n");
+}
+
+void test2() {
+    const int HEIGHT = 5; // 1024;
+    const int WIDTH = 5;  // 1024;
+    const int SIZE = WIDTH * HEIGHT * sizeof(float);
+
+    float *M = new float[SIZE];
+    for (int i = 0; i < HEIGHT * WIDTH; i++) {
+        M[i] = i;
+    }
+
+    for (int i = 0; i < 3; i++) {
+        printf("%f ", M[i]);
+    }
+    printf("\n-------------------\n");
+
+    do_transpose2(M, WIDTH, HEIGHT);
+
+    for (int i = 0; i < 3; i++) {
+        printf("%f ", M[i]);
+    }
+
+    delete (M);
+    printf("\n*** done2 ***\n");
+}
+
+int main(int args, char **vargs) {
+    test1();
+    test2();
+
     return 0;
 }
